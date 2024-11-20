4ef107eb7632826c255598473f36d7288efbca88
traacc <93519551+traacc@users.noreply.github.com>
2024-11-20 13:57:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,16 @@ const swiper = new Swiper('.swiper', {
 
 const consultingBtns = document.querySelectorAll(".container .tabBtn");
 const consultingLists = document.querySelectorAll(".container .consulting__list");
+const consultingItems = document.querySelectorAll(".container .consulting__list .consulting__item");
 const runningLines = document.querySelectorAll(".runningLine .text");
+const clientsRows = document.querySelectorAll(".clients .clients__list .clients__row");
+
+const popupWindow = document.querySelector(".popup");
+const popupWindowHeader = popupWindow.querySelector(".popup__header");
+const popupWindowTitle = popupWindow.querySelector(".title");
+const popupWindowText = popupWindow.querySelector(".popup__content");
+const popupClose = popupWindow.querySelector(".closeBtn");
+const popupOverlay = document.querySelector(".popupOverlay");
 
 consultingBtns.forEach((el, index)=>{
     el.addEventListener('click', ()=>{
@@ -26,4 +35,42 @@ consultingBtns.forEach((el, index)=>{
         consultingLists[index].classList.add('active');
         runningLines[index].classList.add('active');
     });
+});
+consultingItems.forEach((el)=>{
+    el.addEventListener('click', ()=>{
+        popupWindow.classList.add('active');
+        popupOverlay.classList.add('active');
+        popupWindowTitle.textContent = el.querySelector('.title').textContent;
+        popupWindowText.textContent = el.querySelector('.text').textContent;
+        
+        popupWindowHeader.style.backgroundImage = `url("${el.querySelector('.image img').src}")`;
+    });
+});
+popupClose.addEventListener('click', ()=>{
+    popupWindow.classList.remove('active');
+    popupOverlay.classList.remove('active');
+});
+popupOverlay.addEventListener('click', ()=>{
+    popupWindow.classList.remove('active');
+    popupOverlay.classList.remove('active');
+});
+
+function setClientsElements(){
+    let width = window.innerWidth;
+    let firstRow = clientsRows[0].innerHTML;
+    let secondRow = clientsRows[1].innerHTML;
+
+    let countRepeatRows = Math.ceil(width/500);
+
+    for(let i=0; i<countRepeatRows;i++) {
+        firstRow += firstRow;
+        secondRow += secondRow;
+    }
+    console.log(firstRow);
+    clientsRows[0].innerHTML = firstRow;
+    clientsRows[1].innerHTML = secondRow;
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    setClientsElements()
 });
